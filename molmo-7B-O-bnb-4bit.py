bb019b7ace2fff3d3d8ad6521917639cbc56e45c
cyan2k <40628958+cyan2k@users.noreply.github.com>
2024-09-26 00:12:07 +0200

    Create molmo-7B-O-bnb-4bit.py

---
+++ molmo-7B-O-bnb-4bit.py
@@ -0,0 +1,41 @@
+from transformers import (
+    AutoModelForCausalLM,
+    AutoProcessor,
+    GenerationConfig,
+)
+from PIL import Image
+
+
+repo_name = "cyan2k/molmo-7B-O-bnb-4bit"
+arguments = {"device_map": "auto", "torch_dtype": "auto", "trust_remote_code": True}
+
+
+# load the processor
+processor = AutoProcessor.from_pretrained(repo_name, **arguments)
+
+# load the model
+model = AutoModelForCausalLM.from_pretrained(repo_name, **arguments)
+
+
+# load image and prompt
+inputs = processor.process(
+    images=[Image.open("img/luna.jpg")],
+    text="Describe this image.",
+)
+
+
+inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
+
+# generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
+output = model.generate_from_batch(
+    inputs,
+    GenerationConfig(max_new_tokens=200, stop_strings="<|endoftext|>"),
+    tokenizer=processor.tokenizer,
+)
+
+# only get generated tokens; decode them to text
+generated_tokens = output[0, inputs["input_ids"].size(1) :]
+generated_text = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+
+# print the generated text
+print(generated_text)
